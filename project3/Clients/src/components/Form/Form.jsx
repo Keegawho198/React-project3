89a3a396247eb33ccc65c599e5bdd52efb9303be
Keegan Nair <keegan.nair@hotmail.com>
2020-03-27 17:35:04 +1100

    Added dropdown list to form

--- project3/Clients/src/components/Form/Form.jsx
+++ project3/Clients/src/components/Form/Form.jsx
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 
-import _ from 'lodash'; 
+import _ from 'lodash';
 
 import './style.css';
 
 import FormInput from '../FormInput';
 import TableDisplay from '../TableDisplay'
 
+// import { Dropdown, DropdownButton, InputGroup } from 'react-bootstrap';
+//import Select from 'react-select';
+// import Select from "react-dropdown-select";
+import Select from 'react-select'
+
+
+//Options for Dropdown list in form
+const options = [
+  { label: 'One', value: 'One' },
+  { label: 'Two', value: 'Two' },
+  { label: 'Three', value: 'Three' },
+  { label: 'Four', value: 'Four' },
+  { label: 'Five', value: 'Five' },
+  { label: 'Six', value: 'Six' },
+  { label: 'Seven', value: 'Seven' },
+
+];
 
 const Form = () => {
-  // console.log(TableDisplay);
-  //const [name, setName] = useState("");
-  // const [password, setPassword] = useState("");
   const [formData, setFormData] = useState({
     dayNum: "",
     //exerciseId: 0,
@@ -38,50 +52,56 @@ const Form = () => {
     e.preventDefault();
     console.log(formData);
     console.log('saved')
-    // var exerciseId = 0;
-    // console.log(exerciseId);
-    // exerciseId++;
-    // console.log(exerciseId);
 
-    
-    //var exerciseDat = _.omit(formData, 'dayNum')
     setTableData({ ...formData });
   }
 
 
+//This function Handles changes for the dropdown only!!!!
   const handleChange = (e) => {
-    let key = e.target.name;
-    let value = e.target.value;
+    console.log(e);
+    console.log(e.value);
 
+    setFormData({ ...formData, dayNum: e.value })
+  }
+
+  //this handles changes for the other form inputs!
+  const handleChangeSecond = (event) => {
+    let key = event.target.name;
+    let value = event.target.value;
+    // console.log(key);
+    // console.log(value);
     setFormData({ ...formData, [key]: value })
   }
 
-
+  console.log(formData);
   return (
     <>
-    <br></br>
-          
+      <br></br>
 
-  <div className="row">
-    <div className="Form col-sm-4 col-lg-4">
-      <FormInput label="Day Number" type="number" name="dayNum" value={formData.dayNum} onChange={handleChange} />
-      <FormInput label="Focus" type="text" name="focus" value={formData.focus} onChange={handleChange} />
-        <FormInput label="Exercise Name" type="text" name="exerciseName" value={formData.exerciseName} onChange={handleChange} />
-        <FormInput label="Sets" type="number" name="sets" value={formData.sets} onChange={handleChange} />
-        <FormInput label="Reps" type="number" name="reps" value={formData.reps} onChange={handleChange} />
-        <FormInput label="Tempo" type="text" name="tempo" value={formData.tempo} onChange={handleChange} />
-        <FormInput label="Rest" type="number" name="rest" value={formData.rest} onChange={handleChange} />
-        <button type="button" className="btn btn-lg btn-primary " onClick={handleSave}>Save</button>
-    </div>
+      <div className="row">
+        <div className="Form col-sm-4 col-lg-4">
 
-    <div class="col-sm-8 col-md col-lg-8">
-      <TableDisplay dayNum={formData.dayNum} data={tableData}>
-      </TableDisplay>
-    </div>
-  </div>
-      <p>Only submit for one day. Don't add a day 1 and day 2 exercise in one submission</p>
-      
-      
+          <Select label="DayNum" name="dayNum" value={formData.dayNum} options={options}
+            onChange={handleChange} />
+
+          <FormInput label="Focus" type="text" name="focus" value={formData.focus} onChange={handleChangeSecond} />
+          <FormInput label="Exercise Name" type="text" name="exerciseName" value={formData.exerciseName} onChange={handleChangeSecond} />
+          <FormInput label="Sets" type="number" name="sets" value={formData.sets} onChange={handleChangeSecond} />
+          <FormInput label="Reps" type="number" name="reps" value={formData.reps} onChange={handleChangeSecond} />
+          <FormInput label="Tempo" type="text" name="tempo" value={formData.tempo} onChange={handleChangeSecond} />
+          <FormInput label="Rest" type="number" name="rest" value={formData.rest} onChange={handleChangeSecond} />
+          <button type="button" className="btn btn-lg btn-primary " onClick={handleSave}>Save</button>
+        </div>
+
+        <div class="col-sm-8 col-md col-lg-8">
+          <TableDisplay dayNum={formData.dayNum} data={tableData}>
+          </TableDisplay>
+        </div>
+      </div>
+      <p>Only submit for one day! Don't add a day 1 and day 2 exercise in one submission</p>
+
+
 
     </>
 
