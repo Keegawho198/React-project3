671d6e7c7e25bb3231ff9a01afc31f96b70bb49a
Keegan Nair <keegan.nair@hotmail.com>
2020-04-17 18:51:26 +1000

    Alined form input for programs to match logo

--- project3/client/src/pages/Program.js
+++ project3/client/src/pages/Program.js
@@ -1,6 +1,3 @@
-
-
-
 import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 import './style.css';
@@ -143,7 +140,7 @@ const Program = (props) => {
 
       <h1 className="text-center">Program Page</h1>
       <div className="row">
-        <div className="Form col-sm-4 col-lg-4">
+        <div className="Form col-sm-4 col-lg-" id="programForm" style={{marginLeft:'0%', paddingLeft:'7%'}}>
           <p>Day Number</p>
           <Select label="DayNum" name="dayNum" value={options.selected} options={options}
             onChange={handleChange} />
@@ -169,7 +166,7 @@ const Program = (props) => {
           </div>
           <button type="button" className="btn btn-lg btn-primary " onClick={handleSave}>Add</button>
         </div>
-        <div class="col-sm-8 col-md col-lg-8">
+        <div class="col-sm-8 col-md col-lg-">
           <TableDisplay dayNum={formData.dayNum} data={tableData}>
           </TableDisplay>
         </div>
